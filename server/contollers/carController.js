497f5c4a27bb2da4f58720160516ea838d1a8359
LeonidDavidchuk <49954415+LeonidDavidchuk@users.noreply.github.com>
2022-12-23 23:36:33 +0300

    change controllers

--- server/contollers/carController.js
+++ server/contollers/carController.js
@@ -1,6 +1,10 @@
+const uuid = require('uuid')
+
 class CarController {
     async create(req, res) {
-
+        const { model, year, mileage, body, count_size, count_door, type_gas, engine, power,  power_engine, type_of_drive, type_of_gearbox, price } = req.body
+        const { img_1, img_2, img_3, img_4, img_5 } = req.files
+        let fileName = uuid.v4() + ".jpg"
     }
 
     async getAll(req, res) {
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const sequelize = require('./db')
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -10,6 +11,7 @@ const router = require('./routes/index')
 
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 
